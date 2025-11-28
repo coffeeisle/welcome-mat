3b2cabca874b78fdbcda0020cbc1dad6a9e28c39
4ngel2769 <angeldevz0@proton.me>
2025-11-28 20:07:32 +0200

    Add VersionInfo utility class
    Introduces VersionInfo class to centralize configuration and message version constants for easier maintenance and reference.

---
+++ src/main/java/xyz/coffeeisle/welcomemat/VersionInfo.java
@@ -0,0 +1,10 @@
+package xyz.coffeeisle.welcomemat;
+
+public final class VersionInfo {
+    public static final String CONFIG_VERSION = "1.3";
+    public static final String MESSAGES_VERSION = "1.3";
+
+    private VersionInfo() {
+        // Utility class
+    }
+}
